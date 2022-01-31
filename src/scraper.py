17ac1d326e33f004ca1a1b77df36be12e1fc22ad
zoltanp <zoltanp1234@gmail.com>
2022-01-31 16:24:05 +0100

    - made the program actually work

--- src/scraper.py
+++ src/scraper.py
@@ -1,27 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-url='https://www.mtgtop8.com/archetype?a=504&meta=130&f=EDH'
-req = requests.get(url)
-content = req.text
 
-soup = BeautifulSoup(content, features="html.parser")
+# TODO: make program more versatile by adding the url as a param
+def cardScraper():
 
-massiveArray = []
+    #change the number after 'archetype' to the selected one to change which commander is scraped
+    url='https://www.mtgtop8.com/archetype?a=504&meta=130&f=EDH'
+    req = requests.get(url)
+    content = req.text
 
-for i in soup.find_all('tr', attrs={'class':'hover_tr'}):
+    soup = BeautifulSoup(content, features="html.parser")
 
-    #i appended the href because mtgtop8 formatted their hrefs without the base url
-    deckListPageUrl = 'https://www.mtgtop8.com/{href}'.format(href=i.a['href'])
-    deckListPageRequest = requests.get(deckListPageUrl)
-    deckListPageSoup = BeautifulSoup(deckListPageRequest.content, features='html.parser')
+    uniqueCards = set()
+    archetypeName = soup.find('title').text.replace('decklists @ mtgtop8.com', '')
+    for i in soup.find_all('tr', attrs={'class':'hover_tr'}):
 
-    #extracting every unique card and placing it in a list
-    massiveArray.append(deckListPageSoup.find('title').text)
-    massiveArray.append(deckListPageSoup.find_all('span', attrs={'class': 'L14'}).text)
+        #i appended the href because mtgtop8 formatted their hrefs without the base url
+        deckListPageUrl = 'https://www.mtgtop8.com/{href}'.format(href=i.a['href'])
+        deckListPageRequest = requests.get(deckListPageUrl)
+        deckListPageSoup = BeautifulSoup(deckListPageRequest.content, features='html.parser')
 
-text_file = open('Data_Dumps/soup_test_dump.txt', 'w')
+        #extracting every unique card and placing it in the set
+        for j in deckListPageSoup.find_all('span', attrs={'class': 'L14'}):
+            uniqueCards.add(j.text + '\n')
 
-for a in massiveArray:
-    textRow = 'this should be 1 card:{card}\n'.format(card=a)
-    text_file.write(textRow)
+    with open('Data_Dumps/uniqueCards.txt', 'w') as text_file:
+        text_file.write(f'ALL UNIQUE CARDS IN {archetypeName}\n\n')
+        text_file.writelines(uniqueCards)
+
+if __name__ == '__main__':
+    # call the function directly if file is accessed as a script
+    cardScraper()
